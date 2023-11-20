26b1b0f546b3ffeb85731f0619c53e56f66f5df6
Lucky1234V2 <s.chaoui@hetic.net>
2023-11-20 23:19:14 +0100

    test update

--- backend/login.php
+++ backend/login.php
@@ -3,7 +3,7 @@ require 'db.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(isset($data->username) && isset($data->password)){
+if (isset($data->username) && isset($data->password)) {
     $username = $data->username;
     $password = $data->password;
 
@@ -11,9 +11,10 @@ if(isset($data->username) && isset($data->password)){
     $stmt->execute([$username]);
     $user = $stmt->fetch();
 
-    if($user && password_verify($password, $user['password'])){
+    if ($user && password_verify($password, $user['password'])) {
         echo json_encode(["success" => true, "user" => $user]);
     } else {
         echo json_encode(["success" => false]);
     }
 }
+//test
